2f1c8d58d54307fb549a7772a3c64a4dad57e0aa
ShownXu <shownxu79@gmail.com>
2025-02-08 21:38:23 +0800

    测试构造方法

---
+++ src/method/car.java
@@ -0,0 +1,12 @@
+package method;
+
+public class car {
+    String color;
+    int speed;
+    int prise;
+    int seat;
+
+      public car() {
+          System.out.println("测试");
+      }
+}
---
+++ src/method/test.java
@@ -0,0 +1,9 @@
+package method;
+
+public class test {
+    public static void main(String[] args) {
+        car car1 = new car();
+
+
+    }
+}
