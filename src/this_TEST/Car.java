eb97d7c87cda19e47689880f4588d77d1d51f1b6
ShownXu <shownxu79@gmail.com>
2025-02-08 20:37:00 +0800

    this

---
+++ src/this_TEST/Car.java
@@ -0,0 +1,18 @@
+package this_TEST;
+
+public class Car{
+
+        String color;
+        int speed;
+        int seat;
+
+        public void move(){
+            System.out.println(this.color);
+            System.out.println(this.speed);
+            System.out.println("车能动");
+        }
+         public void fly(){
+             System.out.println("车能飞");
+         }
+    }
+
---
+++ src/this_TEST/main.java
@@ -0,0 +1,16 @@
+package this_TEST;
+
+public class main {
+
+    public static void main(String[] args) {
+        Car car = new Car();
+        car.color = "绿色";
+        car.speed = 100;
+        car.seat  = 50;
+        car.move();
+
+        Car car1 = new Car();
+
+
+    }
+}
